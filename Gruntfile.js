808357592683926da4ddff234c3f3e7a8c4362c7
Anisa <anisa.ali7@gmail.com>
2017-04-19 15:22:49 -0500

    controllers and factory

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,8 @@ module.exports = function(grunt){
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       build: {
-        src: 'client/scripts/*.js',
+        src: ['client/scripts/*.js',
+              'client/scripts/**/*.js'],
         dest: 'server/public/scripts/client.min.js'
       }
     },
@@ -38,7 +39,7 @@ module.exports = function(grunt){
     },
     watch: {
       files: [
-        'client/**/*.*'
+        'client/**/*.*',
       ],
       tasks: ['uglify', 'copy']
     }
