3b1c9b7967af6508aabd5a5c6d45f53f2c21ed52
Anisa <anisa.ali7@gmail.com>
2017-04-20 13:44:30 -0500

    bug with balance

--- client/scripts/Factory/MarketService.js
+++ client/scripts/Factory/MarketService.js
@@ -1,33 +1,33 @@
 marketApp.factory('MarketService', [function() {
-  class utilities{
-        constructor(){}
-        static randomNumber (max,min){
-          return ((Math.random() * (max - min ) + min).toFixed(2));
+    class utilities {
+        constructor() {}
+        static randomNumber(max, min) {
+            return ((Math.random() * (max - min) + min).toFixed(2));
         }
     }
+    let baLance = 100;
     const BALANCE = 100;
     const MAXPRICE = 9.99;
     const MINPRICE = 0.50;
     const MAXPRICECHANGE = 0.5;
     const MINPRICECHANGE = -0.5
 
-    class marketItem{
-        constructor(name, price){
-          this.name = name;
-          this.price = price;
-        }//ends contructor
+    class marketItem {
+        constructor(name, price) {
+            this.name = name;
+            this.price = price;
+        } //ends contructor
 
-        changePrice(){
-          let price = this.price + utilities.randomNumber(MAXPRICECHANGE,MINPRICECHANGE);
-          if (price > 9.99){
-            price = 9.99
-          }
-          else if (price < .50){
-            price = .50
-          }
-          this.price = price;
-        }//ends changePrice
-    }//ends marketItem
+        changePrice() {
+            let price = this.price + utilities.randomNumber(MAXPRICECHANGE, MINPRICECHANGE);
+            if (price > 9.99) {
+                price = 9.99
+            } else if (price < .50) {
+                price = .50
+            }
+            this.price = price;
+        } //ends changePrice
+    } //ends marketItem
 
     class UserAcc {
         constructor(balance) {
@@ -41,17 +41,20 @@ marketApp.factory('MarketService', [function() {
         }
 
         DecBal(salePrice) {
+            
+            console.log('User Bal', this.balance);
             this.balance -= salePrice;
+
         }
 
-        CountItem(foo) {
+        CountItem(itemName) {
             let count = 0;
             for (index of this.cart) {
-              console.log("inside CountItem name: ",foo);
-                if (index.name === foo) {
+                if (index === itemName) {
                     count++;
                 }
             }
+            console.log("Count: ", itemName);
             return count;
         }
 
@@ -64,41 +67,39 @@ marketApp.factory('MarketService', [function() {
                     total = total + parseFloat(index.price);
                 }
             }
-            if(count == 0) {
-              avg = 0;
+            if (count == 0) {
+                avg = 0;
             } else {
-              avg = total / count;
+                avg = total / count;
             }
             return avg;
         }
-
-
     } //end UserAcc Class
+
+    //instantiate a user with a balance
+    let user = new UserAcc(baLance);
+
+    // let user = new UserAcc(StartingBalance);
+
+    //list of items which will be instantiated as objects of class marketItem
+    let listOfItems = ['toaster', 'lamp', 'clock', 'blueRay player', 'apples', 'oranges', 'bananas', 'grapes', 'comic books', 'stuffed animals', 'jewelry', 'wine'];
 
     //marketItems array of instantiated objects of class marketItem
     let marketItems = [];
-    //market obect houses the array of marketItems is a portal
-    let market = {};
-
-    //instantiate a user with a balance
-    let user = new UserAcc(BALANCE);
-
-    //list of items which will be instantiated as objects of class marketItem
-    let listOfItems = ['toaster', 'lamp', 'clock', 'blueRay player','apples','oranges','bananas','grapes','comic books','stuffed animals','jewelry','wine'];
-
     //for of loop which instantiates objects of class marketItem based on the listOfItems array
     for (index of listOfItems) {
-      let newItem = new marketItem(index, utilities.randomNumber(MAXPRICE,MINPRICE));
-      marketItems.push(newItem);
+        let newItem = new marketItem(index, utilities.randomNumber(MAXPRICE, MINPRICE));
+        marketItems.push(newItem);
     }
 
-    //adding merketItems array into market object
+    //market object houses the array, marketItems
+    let market = {};
     market.marketItems = marketItems;
+    console.log(market);
     //builds the original summary array
-    //UpdateCartSummary();
+    // UpdateCartSummary();
 
     let sellItem = (item) => {
-      //balance + item.price (from MArketItems)
       user.IncBal(item.price);
       //remove from cart first instance of that item
       for (index of user.cart) {
@@ -107,48 +108,51 @@ marketApp.factory('MarketService', [function() {
               return; // need to jump out of for of loop
           }//end if
       }//ends for loop
-      UpdateCartSummary();
+      // UpdateCartSummary();
     }// end sellItem function
 
     let buyItem = (item) => {
-      //balance - item.price (from MArketItems)
+      // console.log('item to add to Cart', item);
+
       user.DecBal(item.price);
+
       //add item to cart
       user.cart.push(item);
-      UpdateCartSummary();
+      // UpdateCartSummary();
     }// end buyItem function
 
-    let UpdateCartSummary = () => {
-      user.cartSummary = [];
-      let summaryObject;
-      for(let i = 0; i < listOfItems.length; i++) {
-        index = listOfItems[i];
-        summaryObject = {};
-        summaryObject.name = index;
-        // console.log(index);
-          summaryObject.avgPrice = user.PriceAvg(index);
-          summaryObject.count = user.CountItem(index.name);
-          user.cartSummary.push(summaryObject);
-          // console.log("inside UpdateCartSummary", summaryObject, user.cartSummary);
-      }
+    // let UpdateCartSummary = () => {
+    //   user.cartSummary = [];
+    //   let summaryObject = {};
+    //   for(let i = 0; i < listOfItems.length; i++) {
+    //     index = listOfItems[i];
+    //       summaryObject.name = index;
+    //       summaryObject.avgPrice = user.PriceAvg(index);
+    //       summaryObject.count = user.CountItem(index.name);
+    //       user.cartSummary.push(summaryObject);
+    //       // console.log("inside UpdateCartSummary", summaryObject, user.cartSummary);
+    //   }
+    //   console.log(user.cartSummary);
 
-      // for (index of listOfItems) {
-      //   let summaryObject = {};
-      //   summaryObject.name = index;
-      //   summaryObject.avgPrice = user.PriceAvg(index);
-      //   console.log("inside UpdateCartSummary", index);
-      //   summaryObject.count = user.CountItem(index);
-      //   user.cartSummary.push(summaryObject);
-      // }
-    }
+    // for (index of listOfItems) {
+    //   let summaryObject = {};
+    //   summaryObject.name = index;
+    //   summaryObject.avgPrice = user.PriceAvg(index);
+    //   console.log("inside UpdateCartSummary", index);
+    //   summaryObject.count = user.CountItem(index);
+    //   user.cartSummary.push(summaryObject);
+    // }
+    // }
 
 
     return {
+        // balance: BALANCE,
         user: user,
+        balance: user.balance,
         market: market,
         sellItem: sellItem,
         buyItem: buyItem,
-        UpdateCartSummary: UpdateCartSummary
+        // UpdateCartSummary: UpdateCartSummary
 
 
     }
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,19 +2,21 @@ var marketApp = angular.module('marketApp', []);
 
 marketApp.controller('BalanceController', ['$scope', 'MarketService', function($scope, MarketService){
   // temporary variable that stores balance
-  $scope.balance = 100;
+  $scope.balance = MarketService.user.balance;   // this doesn't update as it should...
 
 }]);
 
 marketApp.controller('BuyController', ['$scope', 'MarketService', function($scope, MarketService){
-  $scope.cartSummary = MarketService.user.cartSummary;
+  // $scope.cartSummary = MarketService.user.cartSummary;
 
   $scope.availableItems = MarketService.market.marketItems;
   // MarketService.UpdateCartSummary();
+
   $scope.buyOneItem = function(item) {
-    console.log('buy button clicked, buying one', item);
+    console.log('BUYING ONE:',item.name, 'FOR: $',item.price);
     MarketService.buyItem(item);
-    console.log($scope.cartSummary);
+
+    // console.log($scope.cartSummary);
 
   };
 
@@ -22,11 +24,9 @@ marketApp.controller('BuyController', ['$scope', 'MarketService', function($scop
 
 marketApp.controller('SellController', ['$scope', 'MarketService', function($scope, MarketService){
   // MarketService.UpdateCartSummary();
-  $scope.cartSummary = MarketService.user.cartSummary;
-$scope.sellOneItem = function(item) {
-    console.log('sell button clicked, selling one', item);
-    MarketService.sellItem(item);
-};
-// console.log(sellItem.avgPrice);
-
+//   $scope.cartSummary = MarketService.user.cartSummary;
+// $scope.sellOneItem = function(item) {
+//     // console.log('sell button clicked, selling one', item);
+//     MarketService.sellItem(item);
+// };
 }]);
